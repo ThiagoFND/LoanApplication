b846211f76f726ced661bcca29fbc3462afcd7a4
ThiagoFND <thiagop4t0d4gu4@gmail.com>
2024-02-11 20:24:55 -0300

    Adicionado lombook ao projeto

--- src/main/java/br/com/CIUBank/loan/dto/user/PersonDTO.java
+++ src/main/java/br/com/CIUBank/loan/dto/user/PersonDTO.java
@@ -11,15 +11,20 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import br.com.CIUBank.loan.entity.user.Person;
 import br.com.CIUBank.loan.entity.user.PersonRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Table(name = "person")
 @Entity
+@Setter @Getter @EqualsAndHashCode
 public class PersonDTO implements UserDetails {
 
 	@Id
@@ -43,7 +48,6 @@ public class PersonDTO implements UserDetails {
 	public PersonDTO(String name, String identifier, LocalDate birthDate, String typeIdentifier,
 			BigDecimal minimumMonthlyValueOfInstallments, BigDecimal maximumValueOfAllLoan, String active, String login,
 			String password, PersonRole role) {
-		super();
 		this.name = name;
 		this.identifier = identifier;
 		this.birthDate = birthDate;
@@ -55,178 +59,7 @@ public class PersonDTO implements UserDetails {
 		this.password = password;
 		this.role = role;
 	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getIdentifier() {
-		return identifier;
-	}
-
-	public void setIdentifier(String identifier) {
-		this.identifier = identifier;
-	}
-
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public String getTypeIdentifier() {
-		return typeIdentifier;
-	}
-
-	public void setTypeIdentifier(String typeIdentifier) {
-		this.typeIdentifier = typeIdentifier;
-	}
-
-	public BigDecimal getMinimumMonthlyValueOfInstallments() {
-		return minimumMonthlyValueOfInstallments;
-	}
-
-	public void setMinimumMonthlyValueOfInstallments(BigDecimal minimumMonthlyValueOfInstallments) {
-		this.minimumMonthlyValueOfInstallments = minimumMonthlyValueOfInstallments;
-	}
-
-	public BigDecimal getMaximumValueOfAllLoan() {
-		return maximumValueOfAllLoan;
-	}
-
-	public void setMaximumValueOfAllLoan(BigDecimal maximumValueOfAllLoan) {
-		this.maximumValueOfAllLoan = maximumValueOfAllLoan;
-	}
-
-	public String getActive() {
-		return active;
-	}
-
-	public void setActive(String active) {
-		this.active = active;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public PersonRole getRole() {
-		return role;
-	}
-
-	public void setRole(PersonRole role) {
-		this.role = role;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((active == null) ? 0 : active.hashCode());
-		result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((identifier == null) ? 0 : identifier.hashCode());
-		result = prime * result + ((login == null) ? 0 : login.hashCode());
-		result = prime * result + ((maximumValueOfAllLoan == null) ? 0 : maximumValueOfAllLoan.hashCode());
-		result = prime * result
-				+ ((minimumMonthlyValueOfInstallments == null) ? 0 : minimumMonthlyValueOfInstallments.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((role == null) ? 0 : role.hashCode());
-		result = prime * result + ((typeIdentifier == null) ? 0 : typeIdentifier.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PersonDTO other = (PersonDTO) obj;
-		if (active == null) {
-			if (other.active != null)
-				return false;
-		} else if (!active.equals(other.active))
-			return false;
-		if (birthDate == null) {
-			if (other.birthDate != null)
-				return false;
-		} else if (!birthDate.equals(other.birthDate))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (identifier == null) {
-			if (other.identifier != null)
-				return false;
-		} else if (!identifier.equals(other.identifier))
-			return false;
-		if (login == null) {
-			if (other.login != null)
-				return false;
-		} else if (!login.equals(other.login))
-			return false;
-		if (maximumValueOfAllLoan == null) {
-			if (other.maximumValueOfAllLoan != null)
-				return false;
-		} else if (!maximumValueOfAllLoan.equals(other.maximumValueOfAllLoan))
-			return false;
-		if (minimumMonthlyValueOfInstallments == null) {
-			if (other.minimumMonthlyValueOfInstallments != null)
-				return false;
-		} else if (!minimumMonthlyValueOfInstallments.equals(other.minimumMonthlyValueOfInstallments))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (role != other.role)
-			return false;
-		if (typeIdentifier == null) {
-			if (other.typeIdentifier != null)
-				return false;
-		} else if (!typeIdentifier.equals(other.typeIdentifier))
-			return false;
-		return true;
-	}
-
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 	    return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
--- src/main/java/br/com/CIUBank/loan/entity/loan/Loan.java
+++ src/main/java/br/com/CIUBank/loan/entity/loan/Loan.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import br.com.CIUBank.loan.entity.user.Person;
+import br.com.CIUBank.loan.entity.user.PersonRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,9 +16,13 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Table(name = "Loan")
 @Entity
+@Setter @Getter @EqualsAndHashCode
 public class Loan {
 
 	@Id
@@ -55,115 +60,11 @@ public class Loan {
 			@NotNull(message = "The number of installments cannot be null") Integer numberParcels,
 			@NotNull(message = "Payment status cannot be null") @Size(max = 50, message = "Payment status must be up to 50 characters long") String statusPayment,
 			@NotNull(message = "The creation date cannot be null") LocalDate dateCreation, Person person) {
-		super();
 		this.valueLoan = valueLoan;
 		this.numberParcels = numberParcels;
 		this.statusPayment = statusPayment;
 		this.dateCreation = dateCreation;
 		this.person = person;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public BigDecimal getValueLoan() {
-		return valueLoan;
-	}
-
-	public void setValueLoan(BigDecimal valueLoan) {
-		this.valueLoan = valueLoan;
-	}
-
-	public Integer getNumberParcels() {
-		return numberParcels;
-	}
-
-	public void setNumberParcels(Integer numberParcels) {
-		this.numberParcels = numberParcels;
-	}
-
-	public String getStatusPayment() {
-		return statusPayment;
-	}
-
-	public void setStatusPayment(String statusPayment) {
-		this.statusPayment = statusPayment;
-	}
-
-	public LocalDate getDateCreation() {
-		return dateCreation;
-	}
-
-	public void setDateCreation(LocalDate dateCreation) {
-		this.dateCreation = dateCreation;
-	}
-
-	public Person getPerson() {
-		return person;
-	}
-
-	public void setPerson(Person person) {
-		this.person = person;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dateCreation == null) ? 0 : dateCreation.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((numberParcels == null) ? 0 : numberParcels.hashCode());
-		result = prime * result + ((person == null) ? 0 : person.hashCode());
-		result = prime * result + ((statusPayment == null) ? 0 : statusPayment.hashCode());
-		result = prime * result + ((valueLoan == null) ? 0 : valueLoan.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Loan other = (Loan) obj;
-		if (dateCreation == null) {
-			if (other.dateCreation != null)
-				return false;
-		} else if (!dateCreation.equals(other.dateCreation))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (numberParcels == null) {
-			if (other.numberParcels != null)
-				return false;
-		} else if (!numberParcels.equals(other.numberParcels))
-			return false;
-		if (person == null) {
-			if (other.person != null)
-				return false;
-		} else if (!person.equals(other.person))
-			return false;
-		if (statusPayment == null) {
-			if (other.statusPayment != null)
-				return false;
-		} else if (!statusPayment.equals(other.statusPayment))
-			return false;
-		if (valueLoan == null) {
-			if (other.valueLoan != null)
-				return false;
-		} else if (!valueLoan.equals(other.valueLoan))
-			return false;
-		return true;
 	}
 
 }
